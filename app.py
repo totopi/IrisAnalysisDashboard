8a166398fdc05a0f1e5ec378219054c79fde9351
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-06-08 15:12:58 -0700

    heroku!

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ iris = datasets.load_iris()
 colors = ["#E41A1C", "#377EB8", "#4DAF4A", \
           "#984EA3", "#FF7F00", "#FFFF33", \
           "#A65628", "#F781BF", "#999999"]
-number_of_clusters = range(5)
+number_of_clusters = range(4)
 
 
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
